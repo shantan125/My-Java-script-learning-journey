6ec67e59041be7b8daff57e587fabe5282d1f540
Yeddula Shantan <100433099+shantan125@users.noreply.github.com>
2024-01-20 22:52:47 +0530

    Update script.js
    learned about getAttribute and setAttribute

--- day 5/script.js
+++ day 5/script.js
@@ -1,4 +1,4 @@
-//console.log("hell there ");
+//console.log("hello there ");
 //console.log(window);
 /*window is a global object which consist of lot of functions and methods*/
 // dom is a way of accessing js*/
@@ -29,3 +29,46 @@ divs[2].innerText = "Hey this is box 3";
 //here we are changing the color of h1 element from red to  blue
 let c = document.querySelector("h1");
 c.style.color="blue";
+
+
+// GET ATTRIBUTE AND SET ATTRIBUTE
+
+// DOM (Document Object Model) provides the interface for interacting with HTML documents.
+
+// getAttribute is used to retrieve the value of a specified attribute on an HTML element.
+
+// Syntax of getAttribute: var attributeValue = element.getAttribute(attributeName);
+
+// Example:
+// <div id="exampleDiv" data-info="123">Some content</div>
+// var infoAttributeValue = element.getAttribute('data-info');
+// Outputs: 123
+
+// setAttribute is used to set the value of a specified attribute on an HTML element.
+
+// Syntax of setAttribute: element.setAttribute(attributeName, attributeValue);
+
+// Example:
+// <div id="exampleDiv">Some content</div>
+// element.setAttribute('data-info', '456');
+// After the setAttribute operation, HTML becomes: <div id="exampleDiv" data-info="456">Some content</div>
+
+// These methods are commonly used for dynamic manipulation of HTML elements in web applications.
+
+
+//here we are changing the attribute of the h1 element
+let d = document.querySelector("h4");
+d.setAttribute("class","heading");
+
+// to check the output open your html page in a chrome app and rightclick the page and click on inspect then you can see the changes in the console tab
+//now let us perform on get attribute
+let e = document.querySelector("h4");
+console.log(e.getAttribute("class"));
+//output would be heading
+
+//now let us perform on set attribute
+let f = document.querySelector("h1");
+f.setAttribute("class","heading");
+console.log(f.getAttribute("class"));
+//output would be heading
+
